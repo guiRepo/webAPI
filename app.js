f4b88d65c306f32ce3c804b2e6139a1dfe97ddb3
Guilherme Andre <guilhermeanm95@hotmail.com>
2022-10-29 23:52:21 -0300

    setup app

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+
+
+const app = express();
+
+
+app.use(morgan("dev"))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+
+// setando as mensagens de erro
+app.use((req, res, next) => {
+    const err = new Error(`${req.url} not found in this server`)
+    err.status = 404;
+    next(err);
+})
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({ error: err.message });
+});
+
+module.exports = app;
